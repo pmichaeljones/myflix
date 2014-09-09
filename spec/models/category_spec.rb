44f714c42cddc26e20dc364f55cc5b2fbda90e52
Patrick Jones <pmichaeljones@gmail.com>
2014-09-09 16:38:10 -0600

    implemented shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name:"comedies")
-    category.save
-
-    expect(Category.first).to eq(category)
-  end
 
   it { should have_many(:videos)}
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe Video do
 
-  before do
-    Category.new(name:"Horror").save
-    @video = Video.new(title:"When Harry Met Sally", description:"A touching story.", category_id: 1)
-  end
-
-  it "can be saved" do
-    @video.save
-    expect(@video.valid?).to eq(true)
-  end
-
   it { should belong_to(:category) }
 
   it { should validate_presence_of(:title)}
