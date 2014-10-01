2b60f7e90a944ec9a015364256f6a1c064d0a256
Patrick Jones <pmichaeljones@gmail.com>
2014-09-30 18:13:30 -0600

    finished queue delete action

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Myflix::Application.routes.draw do
 
   resources :users
 
-  resources :queue_items, only: [:create]
+  resources :queue_items, only: [:create, :destroy]
 
   get '/my_queue', to: 'queue_items#index'
 
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -2,6 +2,41 @@ require 'spec_helper'
 
 describe QueueItemsController do
 
+  describe 'DELETE destroy' do
+      let(:diehard) {Fabricate(:video)}
+      let(:jim) {Fabricate(:user)}
+      let(:bob) {Fabricate(:user)}
+
+    it 'should redirect to my queue page' do
+      queue_item = Fabricate(:queue_item)
+      session[:user_id] = jim.id
+      delete :destroy, id: queue_item.id
+      expect(response).to redirect_to my_queue_path
+    end
+
+    it 'deletes the queue item' do
+      queue_item = Fabricate(:queue_item, user: jim, video: diehard)
+      session[:user_id] = jim.id
+      delete :destroy, id: queue_item.id
+      expect(QueueItem.count).to eq(0)
+    end
+
+    it 'should does not delete queue item if current user does not own queue item' do
+      queue_item = Fabricate(:queue_item, user: jim, video: diehard)
+      session[:user_id] = bob.id
+      delete :destroy, id: queue_item.id
+      expect(QueueItem.count).to eq(1)
+    end
+
+
+    it 'redirects to signin page for unauthenticated users' do
+      delete :destroy, id: 1
+      expect(response).to redirect_to sign_in_path
+    end
+
+  end
+
+
   describe 'GET index' do
     it 'sets @queue_items to the queue items of the logged-in user' do
       bob = Fabricate(:user)
