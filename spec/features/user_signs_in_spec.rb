70380c52b24fd513d912b47f4ab7c5d8f6ff6eb7
Patrick Jones <pmichaeljones@gmail.com>
2014-10-08 17:01:09 -0600

    sigin in test #2

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 feature 'User signs in ' do
 
+  background do
+    User.make(:email_address => "p@jones.com", :password => "wally" )
+  end
+
+
   scenario 'with existing username' do
     visit sign_in_path
-    fill_in 'Email', :with => "p@jones.com"
-    fill_in 'Password', :with => 'wally'
+    fill_in 'email', :with => "p@jones.com"
+    fill_in 'password', :with => 'wally'
+    click_button 'Sign in'
 
     page.should have_content("success")
 
