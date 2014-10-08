a30db779ac578a1471af2fbd8e8fdaadbaf8fefb
Patrick Jones <pmichaeljones@gmail.com>
2014-10-08 17:12:14 -0600

    testing suite for user sign in

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,19 +1,34 @@
 require 'spec_helper'
 
 feature 'User signs in ' do
+  let(:bob){ Fabricate(:user) }
 
-  background do
-    Fabricate(:user, :email_address => "p@jones.com", :password => "wally" )
-  end
-
-
-  scenario 'with existing username' do
+  scenario 'with correct email' do
     visit sign_in_path
-    fill_in 'email', :with => "p@jones.com"
-    fill_in 'password', :with => 'wally'
+    fill_in 'email', :with => bob.email_address
+    fill_in 'password', :with => bob.password
     click_button 'Sign in'
 
-    page.should have_content("logged in")
-
+    page.should have_content bob.full_name
   end
+
+  scenario 'with incorrect password' do
+      visit sign_in_path
+      fill_in 'email', :with => bob.email_address
+      fill_in 'password', :with => 'wally'
+      click_button 'Sign in'
+
+      page.should have_content("Your username or password was wrong.")
+  end
+
+  scenario 'with incorrect email' do
+      visit sign_in_path
+      fill_in 'email', :with => "bob@bob.com"
+      fill_in 'password', :with => bob.password
+      click_button 'Sign in'
+
+      page.should have_content("Your username or password was wrong.")
+  end
+
 end
+
