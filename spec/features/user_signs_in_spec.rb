53ccdbf60308d29d52d12e2f34e6bb6f4fd88041
Patrick Jones <pmichaeljones@gmail.com>
2014-10-08 16:48:03 -0600

    sign in test with capybara

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
 
+feature 'User signs in ' do
+
+  scenario 'with existing username' do
+    visit sign_in_path
+    fill_in 'Email', :with => "p@jones.com"
+    fill_in 'Password', :with => 'wally'
+
+    page.should have_content("success")
+
+  end
+end
