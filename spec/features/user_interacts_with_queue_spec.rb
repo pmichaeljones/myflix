d22de49091c63a45c738333ddeb8875060416604
Patrick Jones <pmichaeljones@gmail.com>
2014-10-09 16:03:10 -0600

    finished user interacts with queue test

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -19,6 +19,38 @@ feature "User interacts with the queue" do
     visit video_path(diehard)
     page.should_not have_content("+ My Queue")
 
+    visit root_path
+    find("a[href='/videos/#{anchorman.id}']").click
+    click_link "+ My Queue"
+
+    visit root_path
+    find("a[href='/videos/#{superbad.id}']").click
+    click_link "+ My Queue"
+
+    within(:xpath, "//tr[contains(.,'#{diehard.title}')]") do
+      fill_in "queue_items[][position]", with: 3
+    end
+
+    within(:xpath, "//tr[contains(.,'#{anchorman.title}')]") do
+      fill_in "queue_items[][position]", with: 1
+    end
+
+    within(:xpath, "//tr[contains(.,'#{superbad.title}')]") do
+      fill_in "queue_items[][position]", with: 2
+    end
+
+    # find("input[data-video-id='#{diehard.id}']").set(3)
+    # find("input[data-video-id='#{anchorman.id}']").set(1)
+    # find("input[data-video-id='#{superbad.id}']").set(2)
+
+
+    click_button "Update Instant Queue"
+
+    expect(find("input[data-video-id='#{diehard.id}']").value).to eq("3")
+    expect(find("input[data-video-id='#{anchorman.id}']").value).to eq("1")
+    expect(find("input[data-video-id='#{superbad.id}']").value).to eq("2")
+
+
   end
 
 end
