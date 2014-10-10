d36870d7da3c7cf1026cf4a1c519beab14e4d5d8
Patrick Jones <pmichaeljones@gmail.com>
2014-10-10 17:08:03 -0600

    GET index test for relationships spec

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+describe RelationshipsController do
+
+  describe 'GET index' do
+
+    it 'sets @relationships to the current users following relationships' do
+      bob = Fabricate(:user)
+      set_current_user(bob)
+      john = Fabricate(:user)
+      relationship = Fabricate(:relationship, follower: alice, leader: bob)
+
+      get :index
+      expect(assigns(:relationships)).to eq([relationship])
+    end
+
+  end
+
+
+end
