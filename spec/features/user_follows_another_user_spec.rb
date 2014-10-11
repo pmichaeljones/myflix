1f7b56da98cec7a635208124f904489f2299e23d
Patrick Jones <pmichaeljones@gmail.com>
2014-10-11 15:06:00 -0600

    starting user following feature spec

---
+++ spec/features/user_follows_another_user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'user follows another user' do
+
+  scenario 'user follows user, confirms following status, and then unfollows, confirming status' do
+
+  end
+
+end
