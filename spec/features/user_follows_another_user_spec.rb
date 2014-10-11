22098418a8261f7024c6c201cd5ac3be93839e59
Patrick Jones <pmichaeljones@gmail.com>
2014-10-11 15:09:12 -0600

    added feature spec methods

--- spec/features/user_follows_another_user_spec.rb
+++ spec/features/user_follows_another_user_spec.rb
@@ -3,6 +3,20 @@ require 'spec_helper'
 feature 'user follows another user' do
 
   scenario 'user follows user, confirms following status, and then unfollows, confirming status' do
+    bob = Fabricate(:user)
+    jim = Fabricate(:user)
+
+    sign_in(bob)
+
+    visit_user_profile(jim)
+
+    click_follow_button
+
+    verify_follow_status(bob, jim)
+
+    remove_follow_relationships
+
+    verify_unfollow_status(bob, jim)
 
   end
 
