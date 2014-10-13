986bde92cdf5f8b8cd7e92a5f453b7ef86a71755
Patrick Jones <pmichaeljones@gmail.com>
2014-10-13 15:53:31 -0600

    added hidden field for user token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,11 +1,16 @@
 class PasswordResetsController < ApplicationController
 
   def show
+    @token = params[:id]
     user = User.where(token: params[:id]).first
     redirect_to expired_token_path unless user
   end
 
   def expired
   end
+
+  def create
+  end
+
 
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe PasswordResetsController do
 
   describe "GET show" do
+
+    it 'sets @token' do
+      bob = Fabricate(:user)
+      bob.update_column(:token, "12345")
+      get :show, id: '12345'
+      expect(assigns(:token)).to eq("12345")
+    end
+
     it 'renders show template if the token is valid' do
       bob = Fabricate(:user)
       bob.update_column(:token, "12345")
