c2f1a7e70e73df94674737f2e78c91b513037f15
Patrick Jones <pmichaeljones@gmail.com>
2014-10-14 13:38:21 -0600

    added inviter_id to invitations table

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,10 +1,23 @@
 class InvitationsController < ApplicationController
+  before_filter :require_user
 
   def new
     @invitation = Invitation.new
   end
 
   def create
+    @invitation = Invitation.create(invite_params)
+    @invitation.update_column(:inviter_id, current_user.id)
+    binding.pry
+    redirect_to new_invitation_path
   end
+
+
+  private
+
+  def invite_params
+    params.require(:invitation).permit(:recipient_name, :recipient_email, :message)
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014185958) do
+ActiveRecord::Schema.define(version: 20141014193224) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141014185958) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "inviter_id"
   end
 
   create_table "queue_items", force: true do |t|
