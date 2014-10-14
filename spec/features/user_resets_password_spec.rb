593617908e4e89213a6fdffd6900f8aaf0d9498d
Patrick Jones <pmichaeljones@gmail.com>
2014-10-14 12:29:11 -0600

    implement capybara-email email:

--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -6,10 +6,23 @@ feature 'User resets password' do
     patrick = Fabricate(:user, password: 'old_password')
 
     visit sign_in_path
-    click_link 'Forgot Password'
+    click_link 'Forgot Password?'
     fill_in "Email Address", with: patrick.email_address
     click_button "Send Email"
 
+    open_email(patrick.email_address)
+    current_email.click_link"Reset Password"
+
+    fill_in "New Password", with: "new_password"
+    click_button "Reset Password"
+
+    #save_and_open_page
+
+    fill_in "email", with: patrick.email_address
+    fill_in "password", with: "new_password"
+    click_button "Sign in"
+
+    expect(page).to have_content("Welcome, #{patrick.full_name}")
   end
 
 end
