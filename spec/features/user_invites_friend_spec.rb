113c4290bcd4f3f757bc9f31aa2a3b9da509c419
Patrick Jones <pmichaeljones@gmail.com>
2014-10-15 12:15:18 -0600

    corrected Capybara input field names

--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -8,15 +8,24 @@ feature 'User invites friend' do
 
     visit new_invitation_path
 
-    fill_in "Friend's Name", with: "Mitch Salm"
-    fill_in "Friend's Email Address", with: "mitch@salm.com"
-    fill_in "Message", with: "hey buddy!"
+    fill_in "invitation_recipient_name", with: "Mitch Salm"
+    fill_in "invitation_recipient_email", with: "mitch@salm.com"
+    fill_in "invitation_message", with: "hey buddy!"
     click_button "Send Invitation"
 
     open_email "mitch@salm.com"
     current_email.click_link "Accept This Invitation"
 
+    fill_in "user_password", with: "password"
+    fill_in "user_full_name", with: "Mitch Salm"
 
+    click_button "Sign Up"
+
+    fill_in "email", with: "mitch@salm.com"
+    fill_in "password", with: "password"
+
+    click_link "People"
+    expect(page).to have_content "#{patrick.full_name}"
 
   end
 
