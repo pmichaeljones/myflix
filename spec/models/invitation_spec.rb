8f000059cb611ac0fa7c5a8227c7986fb3ad2fa3
Patrick Jones <pmichaeljones@gmail.com>
2014-10-16 10:57:56 -0600

    changed fabricator from user to invitation in the invitation_spec

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -7,7 +7,7 @@ describe Invitation do
   it { should validate_presence_of(:message) }
 
   it_behaves_like "tokenable" do
-    let(:object){ Fabricate(:user) }
+    let(:object){ Fabricate(:invitation) }
   end
 
 end
