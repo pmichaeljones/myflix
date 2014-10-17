ddc9a77ce3ded7ea1ac18bd485d2541fe37c4e75
Patrick Jones <pmichaeljones@gmail.com>
2014-10-17 13:23:42 -0600

    texting mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
